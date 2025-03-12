ab3a66a5981c91c5fc1619b5030dcaf937cf9edc
perfume <yxll@qql.com>
2025-03-12 20:50:18 +0800

    Fix bugs

--- luasrc/controller/romupdate.lua
+++ luasrc/controller/romupdate.lua
@@ -17,9 +17,9 @@ function action_start_update()
         local mode = uci:get("romupdate", "general", "update_mode") or "keep"
         local script
         if mode == "nokeep" then
-            script = uci:get("romupdate", "general", "update_script_nokeep") or "/usr/share/Lenyu-no-auto.sh"
+            script = uci:get("romupdate", "general", "update_script_nokeep") or "bash /usr/share/Lenyu-no-auto.sh"
         else
-            script = uci:get("romupdate", "general", "update_script_keep") or "/usr/share/Lenyu-auto.sh"
+            script = uci:get("romupdate", "general", "update_script_keep") or "bash /usr/share/Lenyu-auto.sh"
         end
         -- 使用 nohup 后台运行更新脚本，并将日志重定向到 /tmp/romupdate.log（避免页面刷新）
         os.execute("nohup " .. script .. " > /tmp/romupdate.log 2>&1 &")
